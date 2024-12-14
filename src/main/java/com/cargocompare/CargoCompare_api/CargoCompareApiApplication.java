54421ddf457fd4cc4bc286fea4c1a983cb67c7a4
adriDiazz <adriandiazmanzanares9@gmail.com>
2024-12-14 12:56:29 +0100

    creamos el proyecto para la api

---
+++ src/main/java/com/cargocompare/CargoCompare_api/CargoCompareApiApplication.java
@@ -0,0 +1,13 @@
+package com.cargocompare.CargoCompare_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoCompareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoCompareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cargocompare/CargoCompare_api/CargoCompareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cargocompare.CargoCompare_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargoCompareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
